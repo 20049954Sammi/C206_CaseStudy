3759aa86a0ec8948407d60b748f9881eae41c724
user <user@LAPTOP-L4RQ3A0B>
2021-08-02 20:09:46 +0800

    Testing

---
+++ C206_CaseStudy/src/Test_YZ.java
@@ -0,0 +1,27 @@
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * Yi Zheng, Aug 2, 2021 8:09:26 PM
+ */
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * OngYiZheng, Aug 2, 2021 8:09:26 PM
+ */
+
+public class Test_YZ {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
