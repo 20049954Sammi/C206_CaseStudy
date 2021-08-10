58cfeaaa6650723819900a7e9fec392565ee6151
user <user@LAPTOP-L4RQ3A0B>
2021-08-10 23:41:59 +0800

    Create stall class

--- C206_CaseStudy/src/EditStall.java
+++ C206_CaseStudy/src/EditStall.java
@@ -1,9 +1,11 @@
+import java.time.LocalDate;
+
 /**
  * I declare that this code was written by me.
  * I will not copy or allow others to copy my code.
  * I understand that copying code is considered as plagiarism.
  *
- * Yi Zheng, Aug 2, 2021 8:09:26 PM
+ * Yi Zheng, Aug 10, 2021 8:09:26 PM
  */
 
 /**
@@ -16,12 +18,41 @@
 
 public class EditStall {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private int stallId;
+	private String stallName;
+	private LocalDate operationDate;
 
+	public EditStall(int stallId, String stallName, LocalDate operationDate) {
+		this.stallId = stallId;
+		this.stallName = stallName;
+		this.operationDate = operationDate;
 	}
+
+	public int getStallId() {
+		return stallId;
+	}
+
+	public void setStallId(int stallId) {
+		this.stallId = stallId;
+	}
+
+	public String getStallName() {
+		return stallName;
+	}
+
+	public void setStallName(String stallName) {
+		this.stallName = stallName;
+	}
+
+	public LocalDate getOperationDate() {
+		return operationDate;
+	}
+
+	public void setOperationDate(LocalDate operationDate) {
+		this.operationDate = operationDate;
+	}
+
+	
+	
 
 }
