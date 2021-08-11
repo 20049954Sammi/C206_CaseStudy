1c2061638f35008ecb116abb18760de5f5c97f8f
Sammi Tan <Sammi Tan@DESKTOP-K8BV9EK>
2021-08-11 17:21:00 +0800

    PromotionOfferclass-Sammi

---
+++ C206_CaseStudy/src/PromotionOffer.java
@@ -0,0 +1,19 @@
+
+public class PromotionOffer {
+
+	 private int PromotionCode;
+
+	public PromotionOffer(int promotionCode) {
+		
+		PromotionCode = promotionCode;
+	}
+
+	public int getPromotionCode() {
+		return PromotionCode;
+	}
+
+	public void setPromotionCode(int promotionCode) {
+		PromotionCode = promotionCode;
+	} 
+	
+}
